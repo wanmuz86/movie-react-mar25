3a45b0990ac517567236464b768899185a53e1a4
Wan Muzaffar <wanmuz86@gmail.com>
2025-03-25 13:57:51 +0700

    pass data up done - call api done

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,22 @@ import Header from './components/Header'
 import MovieDetail from './components/MovieDetail'
 import MovieList from './components/MovieList'
 import Search from './components/Search'
+import axios from 'axios'
 
 function App() {
 
+ 
+
+  const callApi = async (movieSearch) => {
+    const response = await axios.get(`https://www.omdbapi.com/?s=${movieSearch}&apikey=87d10179`)
+    console.log(response.data)
+    
+  }
 
   return (
     <>
       <Header/>
-      <Search/>
+      <Search handleUserSearch={callApi}/>
       <MovieList/>
       <MovieDetail/>
       <Footer/>
--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from 'react'
 
-const Search = () => {
+const Search = ({handleUserSearch}) => {
     // To retrieve value from input field
     const [movieName,setMovieName] = useState('')
 
@@ -16,7 +16,7 @@ const Search = () => {
         </div>
         <div className='col-3'>
             <button 
-            onClick={(()=>console.log(movieName))}
+            onClick={(()=>handleUserSearch(movieName))}
             className='btn btn-danger' 
             style={{width:'100%'}}>Search for movie</button>
         </div>
